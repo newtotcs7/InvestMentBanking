6f5ce19b0f1660863f910f335c3011f27e206ee7
NeWt <NeWt@DESKTOP-BOKOM75>
2022-09-09 00:04:57 +0530

    OK

--- src/main/java/pom_Classes/Pom3HomePage.java
+++ src/main/java/pom_Classes/Pom3HomePage.java
@@ -7,16 +7,16 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Pom3HomePage {
 
-@FindBy(xpath = "//span[@class='user-id']")private WebElement userID;
-	
+	@FindBy(xpath = "//span[@class='user-id']")private WebElement userID;
+
 	public Pom3HomePage (WebDriver driver) {
 		PageFactory.initElements(driver,this);
 	}
-	
+
 	public String verifyUID() {
 		String actID=userID.getText();
 		return actID;
-		
-		
+
+
 	}
 }
--- src/test/java/Base_Class/BaseClass.java
+++ src/test/java/Base_Class/BaseClass.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseClass {
 
 	public WebDriver driver;
 
 	public void initializeBrowser() {
-		System.setProperty("webdriver.chrome.driver", "F:\\NeWt Workspace\\Ankush_Sir_Selenium\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://kite.zerodha.com/");
